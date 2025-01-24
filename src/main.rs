4de2609c0feebb949182afd95f80882f21c8b089
Jack Maguire <jack@maguire.tech>
2025-01-24 12:52:21 +0700

    fmt + allow CLI params

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::{FuzzySelect, Input};
+use image::{ColorType, DynamicImage, GenericImage, GenericImageView, ImageReader, Pixel, Rgb};
+use indicatif::ProgressBar;
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
-use dialoguer::{FuzzySelect, Input};
-use dialoguer::theme::ColorfulTheme;
-use image::{ColorType, DynamicImage, GenericImage, GenericImageView, ImageReader, Pixel, Rgb};
-use indicatif::{MultiProgress, ProgressBar};
 
 struct Args {
     input: PathBuf,
@@ -13,19 +13,22 @@ struct Args {
     chunks_per_dimension: u32,
     closeness_threshold: u32,
     output_px_size: u32,
-    algorithm: DistanceAlgorithm
+    algorithm: DistanceAlgorithm,
 }
 
 #[derive(Debug, Copy, Clone)]
 pub enum DistanceAlgorithm {
     Euclidean,
-    Manhattan
+    Manhattan,
 }
 
 impl DistanceAlgorithm {
-    fn distance (&self, a: &Rgb<u8>, b: &Rgb<u8>) -> u32 {
+    fn distance(&self, a: &Rgb<u8>, b: &Rgb<u8>) -> u32 {
         #[inline]
-        fn euclidean_distance(Rgb([r, g, b]): &Rgb<u8>, Rgb([cmp_r, cmp_g, cmp_b]): &Rgb<u8>) -> u32 {
+        fn euclidean_distance(
+            Rgb([r, g, b]): &Rgb<u8>,
+            Rgb([cmp_r, cmp_g, cmp_b]): &Rgb<u8>,
+        ) -> u32 {
             let delta_r = r.abs_diff(*cmp_r);
             let delta_g = g.abs_diff(*cmp_g);
             let delta_b = b.abs_diff(*cmp_b);
@@ -34,7 +37,10 @@ impl DistanceAlgorithm {
         }
 
         #[inline]
-        fn manhattan_distance(Rgb([r, g, b]): &Rgb<u8>, Rgb([cmp_r, cmp_g, cmp_b]): &Rgb<u8>) -> u32 {
+        fn manhattan_distance(
+            Rgb([r, g, b]): &Rgb<u8>,
+            Rgb([cmp_r, cmp_g, cmp_b]): &Rgb<u8>,
+        ) -> u32 {
             let delta_r = r.abs_diff(*cmp_r);
             let delta_g = g.abs_diff(*cmp_g);
             let delta_b = b.abs_diff(*cmp_b);
@@ -44,24 +50,80 @@ impl DistanceAlgorithm {
 
         match self {
             Self::Euclidean => euclidean_distance(a, b),
-            Self::Manhattan => manhattan_distance(a, b)
+            Self::Manhattan => manhattan_distance(a, b),
         }
     }
 }
 
 impl Args {
-    pub fn parse () -> anyhow::Result<Self> {
+    pub fn parse() -> anyhow::Result<Self> {
         match Self::parse_env() {
             Some(x) => Ok(x),
-            None => Self::parse_manual()
+            None => Self::parse_manual(),
         }
     }
 
-    fn parse_env () -> Option<Self> {
-        None //TODO: do this
+    fn parse_env() -> Option<Self> {
+        let args: Vec<String> = std::env::args().skip(1).collect();
+
+        if args.len() == 0 {
+            return None;
+        }
+        if args.len() == 1 {
+            let first =args[0].to_lowercase();
+            if ["--help", "-help", "-h", "--h", "help", "h", "?", "-?"].contains(&first.as_str()) {
+                eprintln!("usage: pxls [input_file] [chunks_per_dimension] [closeness_threshold] [distance_algo] [output_file] [output_virtual_pixel_size]");
+                std::process::exit(1);
+            }
+        }
+
+        let Ok(
+            [input, chunks_per_dimension, closeness_threshold, algorithm, output, output_px_size],
+        ): Result<[String; 6], _> = args.try_into()
+        else {
+            return None;
+        };
+
+        let input = PathBuf::from(input);
+        if !input.exists() || !input.is_file() {
+            eprintln!("[input_file] must be a file that exists");
+            return None;
+        }
+
+        let Ok(chunks_per_dimension) = chunks_per_dimension.parse() else {
+            eprintln!("[chunks_per_dimension] must be a valid u32");
+            return None;
+        };
+        let Ok(closeness_threshold) = closeness_threshold.parse() else {
+            eprintln!("[closeness_threshold] must be a valid u32");
+            return None;
+        };
+        let algorithm = match algorithm.to_lowercase().as_str() {
+            "euclidean" => DistanceAlgorithm::Euclidean,
+            "manhattan" => DistanceAlgorithm::Manhattan,
+            _ => {
+                eprintln!("[distance_algo] must be either `euclidean` or `manhattan`");
+                return None;
+            }
+        };
+
+        let output = PathBuf::from(output);
+        let Ok(output_px_size) = output_px_size.parse() else {
+            eprintln!("[output_virtual_pixel_size] must be a valid u32");
+            return None;
+        };
+
+        Some(Self {
+            input,
+            chunks_per_dimension,
+            closeness_threshold,
+            algorithm,
+            output,
+            output_px_size
+        })
     }
 
-    fn parse_manual () -> anyhow::Result<Self> {
+    fn parse_manual() -> anyhow::Result<Self> {
         let theme = ColorfulTheme::default();
         let input = {
             let mut current_dir_fns = vec![];
@@ -83,15 +145,28 @@ impl Args {
 
             current_dir_files.swap_remove(chosen)
         };
-        let chunks_per_dimension = Input::with_theme(&theme).with_prompt("How many chunks per dimension should be used for palette generation?").interact()?;
-        let closeness_threshold = Input::with_theme(&theme).with_prompt("What should the closeness threshold be for palette generation?").interact()?;
-        let algorithm = if FuzzySelect::with_theme(&theme).with_prompt("Which distance algorithm should be used?").items(&["Manhattan (faster)", "Euclidean (more precise)"]).interact()? == 0 {
+        let chunks_per_dimension = Input::with_theme(&theme)
+            .with_prompt("How many chunks per dimension should be used for palette generation?")
+            .interact()?;
+        let closeness_threshold = Input::with_theme(&theme)
+            .with_prompt("What should the closeness threshold be for palette generation?")
+            .interact()?;
+        let algorithm = if FuzzySelect::with_theme(&theme)
+            .with_prompt("Which distance algorithm should be used?")
+            .items(&["Manhattan (faster)", "Euclidean (more precise)"])
+            .interact()?
+            == 0
+        {
             DistanceAlgorithm::Manhattan
         } else {
             DistanceAlgorithm::Euclidean
         };
-        let output: String = Input::with_theme(&theme).with_prompt("What should the output file be?").interact()?;
-        let output_px_size = Input::with_theme(&theme).with_prompt("What should the virtual pixel size be for the output?").interact()?;
+        let output: String = Input::with_theme(&theme)
+            .with_prompt("What should the output file be?")
+            .interact()?;
+        let output_px_size = Input::with_theme(&theme)
+            .with_prompt("What should the virtual pixel size be for the output?")
+            .interact()?;
 
         Ok(Self {
             input,
@@ -99,14 +174,19 @@ impl Args {
             chunks_per_dimension,
             closeness_threshold,
             output_px_size,
-            algorithm
+            algorithm,
         })
     }
 }
 
 fn main() -> anyhow::Result<()> {
     let Args {
-        input, output, chunks_per_dimension, closeness_threshold, output_px_size, algorithm
+        input,
+        output,
+        chunks_per_dimension,
+        closeness_threshold,
+        output_px_size,
+        algorithm,
     } = Args::parse()?;
 
     let image = ImageReader::open(input)?.decode()?;
@@ -127,18 +207,22 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-
-fn get_palette(image: &DynamicImage, chunks_per_dimension: u32, closeness_threshold: u32, dist_algo: DistanceAlgorithm) -> Vec<Rgb<u8>> {
+fn get_palette(
+    image: &DynamicImage,
+    chunks_per_dimension: u32,
+    closeness_threshold: u32,
+    dist_algo: DistanceAlgorithm,
+) -> Vec<Rgb<u8>> {
     let (width, height) = image.dimensions();
     let chunks_per_dimension = chunks_per_dimension.min(width).min(height);
-    let (width_chunk_size, height_chunk_size) = (width / chunks_per_dimension, height / chunks_per_dimension);
+    let (width_chunk_size, height_chunk_size) =
+        (width / chunks_per_dimension, height / chunks_per_dimension);
 
     let max_num_colours = chunks_per_dimension * chunks_per_dimension;
     let progress_bar = ProgressBar::new(max_num_colours as u64);
     let mut av_px_colours = Vec::with_capacity(max_num_colours as usize);
     for chunk_x in 0..chunks_per_dimension {
         for chunk_y in 0..chunks_per_dimension {
-
             let mut map: HashMap<_, u32> = HashMap::new();
             for px_x in (width_chunk_size * chunk_x)..(width_chunk_size * (chunk_x + 1)) {
                 for px_y in (height_chunk_size * chunk_y)..(height_chunk_size * (chunk_y + 1)) {
@@ -169,7 +253,12 @@ fn get_palette(image: &DynamicImage, chunks_per_dimension: u32, closeness_thresh
     av_px_colours
 }
 
-fn convert_to_palette (input: &DynamicImage, palette: &[Rgb<u8>], distance_algorithm: DistanceAlgorithm, scaling_factor: u32) -> DynamicImage {
+fn convert_to_palette(
+    input: &DynamicImage,
+    palette: &[Rgb<u8>],
+    distance_algorithm: DistanceAlgorithm,
+    scaling_factor: u32,
+) -> DynamicImage {
     let (width, height) = input.dimensions();
 
     let (num_width_chunks, num_height_chunks) = (width / scaling_factor, height / scaling_factor);
@@ -192,10 +281,17 @@ fn convert_to_palette (input: &DynamicImage, palette: &[Rgb<u8>], distance_algor
 
             let divisor = (scaling_factor * scaling_factor) as u64;
 
-            let av_px = Rgb([(accum_r / divisor) as u8, (accum_g / divisor) as u8, (accum_b / divisor) as u8]);
-            let chosen_new_colour = palette.iter().copied().min_by_key(|rgb| {
-                distance_algorithm.distance(rgb, &av_px)
-            }).unwrap().to_rgba();
+            let av_px = Rgb([
+                (accum_r / divisor) as u8,
+                (accum_g / divisor) as u8,
+                (accum_b / divisor) as u8,
+            ]);
+            let chosen_new_colour = palette
+                .iter()
+                .copied()
+                .min_by_key(|rgb| distance_algorithm.distance(rgb, &av_px))
+                .unwrap()
+                .to_rgba();
 
             for px_x in (scaling_factor * chunk_x)..(scaling_factor * (chunk_x + 1)) {
                 for px_y in (scaling_factor * chunk_y)..(scaling_factor * (chunk_y + 1)) {
