db5e789f91ca907cf222325b7b9916090d729cf2
Jack Maguire <jack@maguire.tech>
2025-01-24 08:48:09 +0700

    add support for doing multiple algorithms

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,17 @@ fn main() -> anyhow::Result<()> {
     println!("getting palette");
     let av_px_colours = get_av_px_colours(&image);
     println!("found palette");
-    println!("getting output");
-    let output = convert_to_palette(&image, av_px_colours);
-    println!("saved output");
 
-    output.save("output.jpg")?;
+
+    convert_to_palette(&image, &av_px_colours, euclidean_distance)?;
+    convert_to_palette(&image, &av_px_colours, manhattan_distance)?;
 
     Ok(())
 }
 
 
 fn get_av_px_colours (image: &DynamicImage) -> Vec<Rgb<u8>> {
-    const LENGTH_SECTION_SIZE: u32 = 3;
+    const LENGTH_SECTION_SIZE: u32 = 4;
     let (width, height) = image.dimensions();
     let (width_chunk_size, height_chunk_size) = (width / LENGTH_SECTION_SIZE, height / LENGTH_SECTION_SIZE);
 
@@ -53,31 +52,48 @@ fn get_av_px_colours (image: &DynamicImage) -> Vec<Rgb<u8>> {
     av_px_colours
 }
 
-fn convert_to_palette (input: &DynamicImage, palette: Vec<Rgb<u8>>) -> DynamicImage {
+fn convert_to_palette (input: &DynamicImage, palette: &[Rgb<u8>], dist_func: impl Fn(&Rgb<u8>, &Rgb<u8>) -> u32) -> anyhow::Result<()> {
     let mut new_img = DynamicImage::new(input.width(), input.height(), ColorType::Rgb8);
 
+    let fn_name = std::any::type_name_of_val(&dist_func);
+    println!("starting to convert with {fn_name}");
     for x in 0..input.width() {
         if x % 100 == 0 {
             println!("\tprocessing col {}", x + 1);
         }
 
         for y in 0..input.height() {
-            let [r, g, b, _] = input.get_pixel(x, y).0;
+            let px = input.get_pixel(x, y).to_rgb();
 
             let chosen_new_colour = palette.iter().copied().min_by_key(|rgb| {
-                let [cmp_r, cmp_g, cmp_b] = rgb.0;
-
-                let delta_r = r.max(cmp_r) - r.min(cmp_r);
-                let delta_g = g.max(cmp_g) - g.min(cmp_g);
-                let delta_b = b.max(cmp_b) - b.min(cmp_b);
-
-                (delta_r as u32).pow(2) + (delta_g as u32).pow(2) + (delta_b as u32).pow(2)
+                dist_func(rgb, &px)
             }).unwrap();
 
             new_img.put_pixel(x, y, chosen_new_colour.to_rgba());
         }
     }
 
+    println!("finished conversion, saving");
+    new_img.save(format!("{fn_name}.jpg"))?;
+    Ok(())
+}
 
-    new_img
+#[inline]
+#[allow(dead_code)]
+fn euclidean_distance(Rgb([r, g, b]): &Rgb<u8>, Rgb([cmp_r, cmp_g, cmp_b]): &Rgb<u8>) -> u32 {
+    let delta_r = r.max(cmp_r) - r.min(cmp_r);
+    let delta_g = g.max(cmp_g) - g.min(cmp_g);
+    let delta_b = b.max(cmp_b) - b.min(cmp_b);
+
+    (delta_r as u32).pow(2) + (delta_g as u32).pow(2) + (delta_b as u32).pow(2)
+}
+
+#[inline]
+#[allow(dead_code)]
+fn manhattan_distance(Rgb([r, g, b]): &Rgb<u8>, Rgb([cmp_r, cmp_g, cmp_b]): &Rgb<u8>) -> u32 {
+    let delta_r = r.max(cmp_r) - r.min(cmp_r);
+    let delta_g = g.max(cmp_g) - g.min(cmp_g);
+    let delta_b = b.max(cmp_b) - b.min(cmp_b);
+
+    delta_r as u32 + delta_g as u32 + delta_b as u32
 }
